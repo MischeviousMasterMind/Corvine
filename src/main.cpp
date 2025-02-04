08785ed1dbf1a7246a01a70351515c6cd6624a54
MischeviousMasterMind <0KAY04557@gmail.com>
2025-02-04 01:34:23 -0800

    Working on math.cpp, fixing operator==()

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+#include "math.hpp"
+
 int main(){
-    std::cout << "Hello World!" << std::endl;
+
+    int arr[2][2] = {{1, 0}, {0, 1}};
+    math::Matrix<int> m1 = math::Matrix<int>((int *)arr, 2, 2);
+    math::Matrix<int> m2 = math::Matrix<int>((int *)arr, 2, 2);
+
+    if(m1 == m2)
+        std::cout << "True";
+    else
+        std::cout << "False";
+
+    return 0;
 }
